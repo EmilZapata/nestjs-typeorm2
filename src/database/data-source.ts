fc5388680c4290a4eb4ae88b08c79e0737a79b56
Emil Helmut Zapata Arellano <emilzapata95@gmail.com>
2024-03-21 15:34:26 -0500

    feat: 🎸 config name custom for command line

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -19,3 +19,6 @@ export const dataSourceOptions: DataSourceOptions = {
 };
 
 export const AppDataSource = new DataSource(dataSourceOptions);
+
+//"m:g": "npm run typeorm migration:generate ./src/database/migrations/%npm_config_name%",
+// 15:33:41 ❯ npm run m:g --name=create-table-product
